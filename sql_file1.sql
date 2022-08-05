b7fd3a68fb5454d6590ab971c4a556d82a1c80ad
sid508 <75037080+sid508@users.noreply.github.com>
2022-08-05 23:26:59 +0530

    Create sql_file1.sql

---
+++ sql_file1.sql
@@ -0,0 +1,34 @@
+-- INSERT INTO Stock( StockName, CompName, SharePrice, NO_OF_AVAIL_SHARES) VALUES ('MSFT', 'MICROSOFT', 2000, 10000);
+-- INSERT INTO Stock( StockName, CompName, SharePrice, NO_OF_AVAIL_SHARES) VALUES ('RELI', 'RELIANCE', 2300, 10000);
+-- INSERT INTO Stock( StockName, CompName, SharePrice, NO_OF_AVAIL_SHARES) VALUES ('HDB', 'HDFC BANK', 1700, 10000);
+INSERT INTO Stock( StockName, CompName, SharePrice, NO_OF_AVAIL_SHARES) VALUES ('INFY', 'INFOSYS', 1100, 10000);
+INSERT INTO Stock( StockName, CompName, SharePrice, NO_OF_AVAIL_SHARES) VALUES ('BJFIN', 'BAJAJ FINANCE', 1340, 10000);
+INSERT INTO Stock( StockName, CompName, SharePrice, NO_OF_AVAIL_SHARES) VALUES ('ADGE', 'Adani Green Ene', 1990, 10000);
+INSERT INTO Stock( StockName, CompName, SharePrice, NO_OF_AVAIL_SHARES) VALUES ('TITN', 'TITAN', 780, 10000);
+INSERT INTO Stock( StockName, CompName, SharePrice, NO_OF_AVAIL_SHARES) VALUES ('TMAH', 'Tech Mahindra', 1260, 10000);
+INSERT INTO Stock( StockName, CompName, SharePrice, NO_OF_AVAIL_SHARES) VALUES ('BATO', 'BAJAJ AUTO', 900, 10000);
+INSERT INTO Stock( StockName, CompName, SharePrice, NO_OF_AVAIL_SHARES) VALUES ('SMS', 'SIEMENS', 2100, 10000);
+
+
+ SELECT COUNT(*) FROM STOCK;
+
+INSERT INTO Trader( FirstName, LastName, PhoneNo,EmailID,Passwd,City,State) VALUES ('Shailesh', 'Singh', 7701865432, 'Shailesh23@gmail.com','lkjhgfdsa','Delhi','Delhi');
+INSERT INTO Trader( FirstName, LastName, PhoneNo,EmailID,Passwd,City,State) VALUES ('Siddharth', 'Khandelwal', 9608765421, 'Siddharth23@gmail.com','lkjhufdsa','harda','Mp');
+INSERT INTO Trader( FirstName, LastName, PhoneNo,EmailID,Passwd,City,State) VALUES ('Bhanu', 'Rathore',810754255 , 'Bhanu6969@gmail.com','lkjhgfdya','Ratlam','Mp');
+INSERT INTO Trader( FirstName, LastName, PhoneNo,EmailID,Passwd,City,State) VALUES ('Vansh', 'Mittal', 7701565432, 'vansh3@gmail.com','lkjhggdsa','Agra','UP');
+
+-- -- select * from Trader;
+
+
+INSERT INTO Account_(UserId) VALUES (1);
+INSERT INTO Account_(UserId) VALUES (2);
+INSERT INTO Account_(UserId) VALUES (3);
+INSERT INTO Account_(UserId) VALUES (4);
+
+select * from Account_;
+select * from Trader;
+
+select * from Trader;
+select * from Portfolio;
+
+select * from Stock;
